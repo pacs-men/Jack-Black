3321467ce36054f6242d75e56c79d96398f158a6
pacs-men <cemeric22@gmail.com>
2018-10-18 11:04:21 +0200

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,17 +1,40 @@
-def valueAce():
-    print(""
+import random
+
+
+
+#This file includes all of the consants and basic fonction of the game
 
 
 
 
 colors = ["Clubs", "Diamonds", "Hearts", "Spades"]
-value  = {"2":2, "3":3, "4":4,"5":5, "6":6 , "7":7,
-          "8":8, "9":9, "10":10, "Jack":10, "Queen":10, "King":10, "Ace":valueAce()}
+values  = {"2":2, "3":3, "4":4,"5":5, "6":6 , "7":7,
+          "8":8, "9":9, "10":10, "Jack":10, "Queen":10, "King":10, "Ace":0}
 
 
+def deck():
+    dek = []
+    for color in colors:
+          for value in values:
+              dek.append(value+ " of "+color)
+
+    return dek
 
 
+def valueCard(card):
+    return values[card.split()[0]]
 
-def paquet():
+
+def initdraw(n):
+    dek = []
+    for i in range(n):
+        dek.append(deck())
+        random.shuffle(dek[i])
+    return dek
+
+def draw_card()
+
+if __name__ == "__main__":
+    print(initdraw(1))
     
-    
+
