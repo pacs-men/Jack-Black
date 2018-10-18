749b47b7b13ed81885b56f76195f39f5f1b33e0e
pacs-men <cemeric22@gmail.com>
2018-10-18 10:27:02 +0200

    Add files via upload

---
+++ fonctions.py
@@ -0,0 +1,17 @@
+def valueAce():
+    print(""
+
+
+
+
+colors = ["Clubs", "Diamonds", "Hearts", "Spades"]
+value  = {"2":2, "3":3, "4":4,"5":5, "6":6 , "7":7,
+          "8":8, "9":9, "10":10, "Jack":10, "Queen":10, "King":10, "Ace":valueAce()}
+
+
+
+
+
+def paquet():
+    
+    
