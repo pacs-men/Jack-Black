bab5301a3691affb5e8aefa818b834a6f40a3df4
pacs-men <cemeric22@gmail.com>
2018-12-10 11:09:49 +0100

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,44 +1,44 @@
 from fonctions import *
 import random
    
+import matplotlib.pyplot as plt
+plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
 
 #AI with random choice
 def rand(cards, dealer,  deck, param):
     return random.choice([True,  False])
 
-#AI that draws cards until a caertain number param is reaced
+
+# RANDOM AI that a param probablity of stopping at each turn
+def rand_param(cards, dealer,  deck, param):
+    r = random.random()
+    if r>param:
+        return True
+    else:
+        return False
+
+#AI that draws cards until a caertain number param is reached
 def stop_nb(cards, dealer, deck,  param):
     if score(cards)>= param:
         return False
     else:
         return True
 
+#semi random AI whose probability of drawing is determined with its distance to 21
+def rand_dist_21(cards, dealer, deck,  param):
+    dist = 21 - score(cards)
+    r = random.random()*21
+    if r>dist:
+        return False
+    else:
+        return True
 
-#AI that know all the remaining cards and calculates probabilities to make the best decision
-def prob_bust(cards, dealer, deck,  param):
-    # creates a dictionnary linking the values of the cards with the
-    # number of cards having this value
-    instances = dict([(i, 0) for i in range(1, 11)])
-    for c in cards:
-        instances[valueCard(c)] += 1
-        
-
-    # transfrom the instances dict to probabilities
-    probas = dict(instances)
-    for p in probas:
-        probas[p] = probas[p]/len(cards)
-
-    sc = score(cards)
-    reach = 21-sc
-
-    p_bust = 0
-    for i in range(reach, 11):
-        try:
-            
     
-        
-    
-
 # calculates the expectency of different AI functions using n decks of cards
 # over iterations
 def prob_win(AI, n, iterations, param = 0):
@@ -72,10 +72,33 @@ def prob_win(AI, n, iterations, param = 0):
 
     return (wins+ 0.5*draws)/iterations
 
+
+def matplotgraph(inputs, values):
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    ax.barh(inputs, values, align='center', color='red')
+    plt.show()
+
+
+#print(prob_win(rand_dist_21, 1, 10000))
+
+
+
+"""
+for i in range(0, 11):
+    print("For i = ", i/10, "The expectency is:",prob_win(rand_param, 1, 10000, i/10))
+"""
+"""
+I = []
+prob = []
 for i in range(1, 22):
-    print("For i = ", i, "The expectency is:",prob_win(stop_nb, 1, 10000, i)) 
-    
-    
+    I.append(i)
+    prob.append(prob_win(stop_nb, 1, 1000, i))
+    print("For i = ", i, "The expectency is:",prob[-1])
+
+matplotgraph(I, values)
+"""
     
 
     
